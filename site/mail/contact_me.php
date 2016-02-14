732979eccc3ecdf564c4e10b8aeb72d1b9b76a03
carlos <carlos@debian.lan>
2016-02-14 15:55:18 -0600

    Agregando nuevo destinatario v2

--- site/mail/contact_me.php
+++ site/mail/contact_me.php
@@ -21,7 +21,7 @@ $message = $_POST['message'];
 $to = 'roberto@lomar-gcg.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nZip Code:\n$zip \n\nMessage:\n$message";
-$headers = "From: info@lomar-gcg.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: roberto@lomar-gcg.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
