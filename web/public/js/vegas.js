4e2f5ec3f2fdca9a0ce9ae2920e4cf76025e2082
Víctor M. Reyes <oicrruf.0286@gmail.com>
2017-02-23 23:04:51 -0600

    LoMar | 75%

--- web/public/js/vegas.js
+++ web/public/js/vegas.js
@@ -14,9 +14,9 @@ $(window).load(function() {
 
         $('.overlay').vegas({
             slides: [
-                { src: 'img/slide-1.jpg' },
-                { src: 'img/slide-2.jpg' },
-                { src: 'img/slide-3.jpg' },
+                { src: 'public/img/slide-1.jpg' },
+                { src: 'public/img/slide-2.jpg' },
+                { src: 'public/img/slide-3.jpg' },
             ],
 
             // Delay beetween slides in milliseconds.
