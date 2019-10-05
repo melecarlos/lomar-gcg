099464b6733ecddf414b4083c3cd534ebf9c4582
Víctor M. Reyes <vm.reyesal@gmail.com>
2019-10-04 23:04:13 -0600

    Cambios previos a nueva versión en .NET Core

--- README.md
+++
@@ -1,4 +0,0 @@
-lomar-gcg
-=========
-
-A Symfony project created on October 5, 2016, 6:28 pm.
--- src/LOMAR/WebBundle/LOMARWebBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace LOMAR\WebBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class LOMARWebBundle extends Bundle
-{
-}
