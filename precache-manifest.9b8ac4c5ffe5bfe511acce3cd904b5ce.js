4af79a518fcaca68389e237e3c288914c75a193e
Víctor M. Reyes <vm.reyesal@gmail.com>
2020-04-26 10:41:39 -0600

    Updates

---
+++ precache-manifest.9b8ac4c5ffe5bfe511acce3cd904b5ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "480116da8590d5bc5a85d08d62d67324",
+    "url": "/lomar-gcg/index.html"
+  },
+  {
+    "revision": "10fc0fb25b532a704f58",
+    "url": "/lomar-gcg/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e8a338383cd9a49bf96b",
+    "url": "/lomar-gcg/static/js/2.f1020b2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lomar-gcg/static/js/2.f1020b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10fc0fb25b532a704f58",
+    "url": "/lomar-gcg/static/js/main.abdca15f.chunk.js"
+  },
+  {
+    "revision": "5e51de84022a7549a715",
+    "url": "/lomar-gcg/static/js/runtime-main.b5f9e240.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lomar-gcg/static/media/logo.5d5d9eef.svg"
+  }
+]);
