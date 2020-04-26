936d5b95d4bba8fb60a5719a47a4184a70491d16
Víctor M. Reyes <vm.reyesal@gmail.com>
2020-04-26 11:19:41 -0600

    Init React App

---
+++ src/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+//import Articulos from './components/Articulos';
+
+export default [
+//   <React.Fragment key={"Routes"}>
+//       <Route exact path="/" component={ Home } ></Route>
+//   </React.Fragment>
+];
